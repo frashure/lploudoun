bde5c5ec60ddc28a1c3f598cf9ea95afe760bb08
Chris Frashure <csfrashure@gmail.com>
2019-06-10 20:01:02 -0400

    replaced default app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/lp_logo_small.png';
+import lplcLogo from './images/lplc.png';
 import './App.css';
+import Signup from './Signup';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={lplcLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+    <Signup />
     </div>
   );
 }
