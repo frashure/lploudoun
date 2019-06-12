db6d2a19368c6af39cb7c8d9988c5b9e50d85ff4
Chris Frashure <csfrashure@gmail.com>
2019-06-11 21:56:46 -0400

    added props, state, Greeting component, conditional render

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import logo from './images/lp_logo_small.png';
 import lplcLogo from './images/lplc.png';
 import './App.css';
 import Signup from './Signup';
+import Greeting from './Greeting.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={lplcLogo} className="App-logo" alt="logo" />
-        <p>
-          Coming soon!
-        </p>
-      </header>
-    <Signup />
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      signup: false
+    }
+    this.signupHandler = this.signupHandler.bind(this)
+  }
+
+  signupHandler() {
+    this.setState({
+      signup: false
+    })
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={lplcLogo} className="App-logo" alt="logo" />
+            <h3>
+              Coming soon!
+            </h3>
+          </header>
+          {this.state.signup ? <Signup /> : <Greeting handler = {this.signupHandler} />}
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
