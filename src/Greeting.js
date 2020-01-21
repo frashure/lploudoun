24c1e0a2f27dae375614b19a156f6433f53f0851
Chris Frashure <csfrashure@gmail.com>
2020-01-20 20:03:25 -0500

    changed greeting message

--- src/Greeting.js
+++ src/Greeting.js
@@ -5,8 +5,10 @@ class Greeting extends React.Component {
     render() {
         return (
         <div className="Greeting">
-                Sign up to be notified of any LPLC news/events!<br /><br />
-                <button id="signup" onClick={this.props.handler}>Click here!</button>
+            The Loudoun County LP Committee is preparing for a big year in 2020.
+            <br />
+            Sign up for our email list to stay notified of news and events.<br /><br />
+            <button id="signup" onClick={this.props.handler}>Click here!</button>
         </div>
     )
     }
