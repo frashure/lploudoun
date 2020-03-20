d886774fdfaef9da70a8e37d94c4d8343a195655
Chris Frashure <csfrashure@gmail.com>
2020-03-19 20:36:08 -0400

    passed json body to stringify

--- src/Signup.js
+++ src/Signup.js
@@ -16,6 +16,7 @@ class Signup extends React.Component {
   this.handleChangeEmail = this.handleChangeEmail.bind(this);
   this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
   this.handleChangeLastName = this.handleChangeLastName.bind(this);
+  this.handleChangeResident = this.handleChangeResident.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
 
 }
@@ -32,13 +33,23 @@ handleChangeEmail(e) {
   this.setState({email: e.target.value});
 }
 
+handleChangeResident(e) {
+  this.setState({resident: e.target.value});
+}
+
 handleSubmit(e) {
   e.preventDefault();
 
-  fetch('https://api.virginiaelects.com/lploudoun/first/' + this.state.firstName + '/last/' + this.state.lastName + '/email/' + this.state.email, {
+  fetch('https://api.virginiaelects.com/lploudoun/', {
   method: 'POST',
+  body: JSON.stringify({
+    'fName': this.state.firstName,
+    'lName': this.state.lastName,
+    'email': this.state.email,
+    'resident': this.state.resident
+  }),
   headers: {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'application/json',
   }
 })
 
@@ -47,7 +58,8 @@ handleSubmit(e) {
     'Email: ' + this.state.email + '\n' +
     'First name: ' + this.state.firstName + '\n' +
     'Last name: ' + this.state.lastName + '\n' +
-    'Submitted: ' + this.state.submitted)
+    'Submitted: ' + this.state.submitted + '\n' +
+    'Resident:' + this.state.resident)
 }
 
 render() {
@@ -60,6 +72,11 @@ render() {
             <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChangeLastName}></input>
             <label htmlFor="email">Email: </label>
             <input type="text" name="email"  value={this.state.email} onChange={this.handleChangeEmail}></input>
+            <label htmlFor="resident">Are you a resident of Loudoun?</label>
+            <select name="resident" onChange={this.handleChangeResident}>
+              <option value="T">Yes</option>
+              <option value="F">No</option>
+              </select> 
             <input type="submit" value="Submit" />
         </form>
       </div>
