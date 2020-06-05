6ffa7c29f4437ff742c7fde8a23e82ff21584127
Chris <csfrashure@gmail.com>
2020-06-05 17:39:25 -0400

    match official LP HEX colors

--- src/Greeting.js
+++ src/Greeting.js
@@ -6,9 +6,11 @@ class Greeting extends React.Component {
     render() {
         return (
         <div className="Greeting">
-            The Loudoun County LP Committee is preparing for a big year in 2020.
+            <h3>Join us, and help fight for freedom, individualism, and peace.</h3><br /><br />
+
+            {/* The Loudoun County LP Committee is preparing for a big year in 2020.
             <br />
-            Sign up for our email list to stay notified of news and events.<br /><br />
+            Sign up for our email list to stay notified of news and events.<br /><br /> */}
             <button id="signup" onClick={this.props.handler}>Sign Up</button>
         </div>
     )
